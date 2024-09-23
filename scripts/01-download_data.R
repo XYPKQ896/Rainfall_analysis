3e40b64cff14c2289428c34a5d78167a1ff70b5c
XYPKQ896 <hr.hu@mail.utoronto.ca>
2024-09-22 21:16:36 -0400

    Download raw data.

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,5 +1,4 @@
 #### Preamble ####
-<<<<<<< HEAD
 # Purpose: Downloads and saves the data from Open Data Toronto
 # Author: Rui Hu
 # Date: 22 September 2024
@@ -7,21 +6,11 @@
 # License: MIT
 # Pre-requisites: None
 # Any other information needed? None
-=======
-# Purpose: Downloads and saves the data from [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
->>>>>>> 4975f1b355327449de4b58c2667db3b8385bb1f7
 
 
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-<<<<<<< HEAD
 library(dplyr)
 
 # get package
@@ -48,18 +37,4 @@ summary(raw_data$Total_Rainfall)
 
 #### Save data ####
 write_csv(raw_data, "data/raw_data/raw_data.csv")
-=======
-# [...UPDATE THIS...]
 
-#### Download data ####
-# [...ADD CODE HERE TO DOWNLOAD...]
-
-
-
-#### Save data ####
-# [...UPDATE THIS...]
-# change the_raw_data to whatever name you assigned when you downloaded it.
-write_csv(the_raw_data, "inputs/data/raw_data.csv") 
-
-         
->>>>>>> 4975f1b355327449de4b58c2667db3b8385bb1f7
