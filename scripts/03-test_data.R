c612d16abaa72b79f938dfe353d17017a9e8b1af
XYPKQ896 <hr.hu@mail.utoronto.ca>
2024-09-23 14:13:25 -0400

    Test simulated data.

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,15 +1,22 @@
 #### Preamble ####
-# Purpose: Tests... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Sanity check of the data
+# Author: Rui Hu
+# Date: 23 September 2024
+# Contact: hr.hu@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Need to have simulated data
+# Any other information needed? None.
 
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+
 
 #### Test data ####
+data <- read_csv("data/raw_data/simulated.csv")
+
+# Test for negative numbers
+data$rainfall |> min() <= 0
+
+# Test for NAs
+all(is.na(data$rainfall))
